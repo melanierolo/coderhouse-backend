223118c95638739fa0ccf54f79b49e465aa5a695
Melanie Rodas <mrodasl@uni.pe>
2023-08-22 18:02:18 -0500

    agregando el test de addProduct()

--- desafio-1/index.js
+++ desafio-1/index.js
@@ -1,9 +1,35 @@
 class ProductManager {
   constructor() {
-    this.product = [];
+    this.products = [];
   }
 
   addProduct(title, description, price, thumbnailImg, code, stock) {
-    this.product.push({ title, description, price, thumbnailImg, code, stock });
+    this.products.push({
+      title,
+      description,
+      price,
+      thumbnailImg,
+      code,
+      stock,
+    });
+  }
+
+  getProduct() {
+    return this.products;
   }
 }
+
+// Probando
+
+const products = new ProductManager();
+
+products.addProduct(
+  'producto prueba',
+  'Este es un producto prueba',
+  200,
+  'Sin imagen',
+  'abc123',
+  25
+);
+
+console.log(products.getProduct());
