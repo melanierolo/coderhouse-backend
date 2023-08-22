a90088ce26901436c8ff8482ad7acff318458ecb
Melanie Rodas <mrodasl@uni.pe>
2023-08-22 17:55:04 -0500

    agregar método addProduct

--- desafio-1/index.js
+++ desafio-1/index.js
@@ -2,4 +2,8 @@ class ProductManager {
   constructor() {
     this.product = [];
   }
+
+  addProduct(title, description, price, thumbnailImg, code, stock) {
+    this.product.push({ title, description, price, thumbnailImg, code, stock });
+  }
 }
