2c15bccf8ec70f0701f73be8e675d8427a7ecd40
Melanie Rodas <mrodasl@uni.pe>
2023-08-22 18:47:30 -0500

    agregando la validación de que no se repita el campo code

--- desafio-1/index.js
+++ desafio-1/index.js
@@ -7,16 +7,23 @@ class ProductManager {
   static id = 0;
 
   addProduct(title, description, price, thumbnailImg, code, stock) {
-    ProductManager.id += 1;
-    this.products.push({
-      title,
-      description,
-      price,
-      thumbnailImg,
-      code,
-      stock,
-      id: ProductManager.id,
-    });
+    // Verificar si el code del producto ya existe
+    const codeExists = this.products.some((product) => product.code === code);
+
+    if (title && description && price && code && !codeExists) {
+      ProductManager.id += 1;
+      this.products.push({
+        title,
+        description,
+        price,
+        thumbnailImg,
+        code,
+        stock,
+        id: ProductManager.id,
+      });
+    } else {
+      console.log('Error: Inválido o datos duplicados');
+    }
   }
 
   getProduct() {
@@ -61,3 +68,13 @@ console.log(productsTwo.getProduct());
 // Test - ID
 console.log(products.getProductById(3));
 console.log(products.getProductById(1));
+
+// Test - Añadir producto con el code duplicado
+products.addProduct(
+  'producto repetido',
+  'Este es un producto repetido',
+  150,
+  'Otra imagen',
+  'abc123', //  repetido
+  15
+);
